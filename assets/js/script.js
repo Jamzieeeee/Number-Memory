bf28ac34f7eaa68627cec3eefe579a892f0a3d68
James Whitaker <james@altrion.org>
2024-05-10 14:47:09 +0000

    Change level splash to number splash, update README with current bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,59 @@
+// show element function
+function showElement(elementId) {
+    var element = document.getElementById(elementId);
+    element.style.display = 'block';
+}
+
+// hide element function
+function hideElement(elementId) {
+    var element = document.getElementById(elementId);
+    element.style.display = 'none';
+}
+
+// show then hide element function
+function showElementFor3Seconds(elementId, callback) {
+    // show the element
+    showElement(elementId);
+
+    // wait for 3 seconds
+    setTimeout(function() {
+        // hide the element after 3 seconds
+        hideElement(elementId);
+        // Execute the callback function after hiding the element
+        if (callback && typeof callback === 'function') {
+            callback();
+        }
+    }, 3000); // 3000 milliseconds = 3 seconds
+}
+
+let level = 5
+
+let randomNumber = 0
+
+function generateNumber() {
+    randomNumber = Math.floor(Math.random() * (10 ** level))
+}
+
+// show random number in html span
+document.getElementById("random-number").textContent = randomNumber;
+
+// next level function
 function nextLevel() {
-    // hide element function
-    function hideElement(elementId) {
-        var element = document.getElementById(elementId);
-        element.style.display = 'none';
-    }
-
-    // show element function
-    function showElement(elementId) {
-        var element = document.getElementById(elementId);
-        element.style.display = 'block';
-    }
-
     // hide intro screen
     hideElement('intro-area');
 
     // hide game area
     hideElement('game-area');
 
-    // show then hide level number
-    function showElementFor3Seconds(elementId, callback) {
-        // show the element
-        showElement(elementId);
-
-        // wait for 3 seconds
-        setTimeout(function() {
-            // hide the element after 3 seconds
-            hideElement(elementId);
-            // Execute the callback function after hiding the element
-            if (callback && typeof callback === 'function') {
-                callback();
-            }
-        }, 3000); // 3000 milliseconds = 3 seconds
-    }
-
     // callback function to show the game area after the level-splash
     function showGameArea() {
         showElement('game-area');
     }
 
-    // show level splash and then show game area
-    showElementFor3Seconds('level-splash', showGameArea);
-}
+    // generate new number
+    generateNumber();
+    console.log(randomNumber);
 
-let level = 1
-
-function generateNumber() {
-    Math.floor(Math.random() * (10 ** level))
+    // show random number splash and then show game area
+    showElementFor3Seconds('number-splash', showGameArea);
 }
